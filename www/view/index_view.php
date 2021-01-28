0ee1f4083cc7c3d8f0e522232e5d6ab04f29d50b
Sho Nakao <regza.nakao@gmail.com>
2021-01-29 00:49:54 +0900

    #3エスケープ処理及びコメント追記

--- www/view/index_view.php
+++ www/view/index_view.php
@@ -1,17 +1,21 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
+  <!-- <head>の共通部分を読み込み -->
   <?php include VIEW_PATH . 'templates/head.php'; ?>
   
   <title>商品一覧</title>
-  <link rel="stylesheet" href="<?php print(STYLESHEET_PATH . 'index.css'); ?>">
+  <!-- HOME画面に関係するCSSを読み込み -->
+  <link rel="stylesheet" href="<?php print(h(STYLESHEET_PATH . 'index.css')); ?>">
 </head>
 <body>
+  <!-- headerの共通部分を読み込み -->
   <?php include VIEW_PATH . 'templates/header_logined.php'; ?>
   
 
   <div class="container">
     <h1>商品一覧</h1>
+    <!-- messagesの共通部分を読み込み -->
     <?php include VIEW_PATH . 'templates/messages.php'; ?>
 
     <div class="card-deck">
@@ -20,16 +24,16 @@
         <div class="col-6 item">
           <div class="card h-100 text-center">
             <div class="card-header">
-              <?php print($item['name']); ?>
+              <?php print(h($item['name'])); ?>
             </div>
             <figure class="card-body">
-              <img class="card-img" src="<?php print(IMAGE_PATH . $item['image']); ?>">
+              <img class="card-img" src="<?php print(h(IMAGE_PATH . $item['image'])); ?>">
               <figcaption>
-                <?php print(number_format($item['price'])); ?>円
+                <?php print(h(number_format($item['price']))); ?>円
                 <?php if($item['stock'] > 0){ ?>
                   <form action="index_add_cart.php" method="post">
                     <input type="submit" value="カートに追加" class="btn btn-primary btn-block">
-                    <input type="hidden" name="item_id" value="<?php print($item['item_id']); ?>">
+                    <input type="hidden" name="item_id" value="<?php print(h($item['item_id'])); ?>">
                   </form>
                 <?php } else { ?>
                   <p class="text-danger">現在売り切れです。</p>
